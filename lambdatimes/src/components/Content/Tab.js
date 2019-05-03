f1af5293f7c784f157076a45fe967f23faa38bb2
Gustavo Isturiz <isturizgustavo@gmail.com>
2019-05-03 12:17:45 -0400

    Card/s, Tab/s, and content displaying

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
 
@@ -6,21 +7,27 @@ const Tab = props => {
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
 
-  if (props.tab)
+  const activeTab = props.tab === props.selectedTab;
+
   return (
     <div
-      className={props.selected === props.selectedTab ? 'tab active-tab' : 'tab'}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-         props.selectTabHandler(props.tab)
-      }}
-    >
+      className={`tab ${activeTab ? 'active-tab' : ''}`}
+      onClick={() => {props.selectTabHandler(props.tab)}} >
+
+        {/* Replace this dummy click handler function with your selectTabHandler function from props 
+         you'll need to pass the `tab` in as an argument to this handler.  */}
+        
       {props.tab.toUpperCase()}
     </div>
   );
 };
 
 // Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  tab: PropTypes.string,
+};
 
 export default Tab;
